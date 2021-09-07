485d45e3546994a251c419408adeaf3a7901ae37
Jasper Francisco <jasper.francisco@gmail.com>
2021-09-07 20:19:26 +0800

    Add bill calculation methods to Cart class
    Also crafted a Bill class to represent a calculated bill

---
+++ src/CartApi/Data/Bill.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CartApi.Data
+{
+	public class Bill
+	{
+		public string Currency { get; private set; }
+		public decimal Subtotal { get; private set; }
+		public decimal Taxes { get; private set; }
+		public IDictionary<string, decimal> Discounts { get; private set; }
+		public decimal Total { get; private set; }
+
+		// Limitation: No validation logic especially for object state (e.g. Subtotal - Taxes - Discounts = Total)
+		public Bill(string currency, decimal subtotal, decimal taxes, IDictionary<string, decimal> discounts, decimal total)
+		{
+			Currency = currency;
+			Subtotal = subtotal;
+			Taxes = taxes;
+			Discounts = discounts;
+			Total = total;
+		}
+	}
+}
--- src/CartApi/Data/Cart.cs
+++ src/CartApi/Data/Cart.cs
@@ -1,11 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CartApi.Data
 {
 	public class Cart
 	{
-		public int Id { get; set; }
-		public IDictionary<Product, int> ProductCounts { get; set; }
+		private const decimal TAX_RATE = 0.14M;
+		public ICollection<Product> Products { get; set; } = new List<Product>();
+
+        public Bill CalculateBill()
+		{
+			decimal subtotal = decimal.Zero;
+			// Get subtotal by summing up the products' prices
+			subtotal += Products.Select(p => p.Price).Sum();
+			
+			// Get tax amount according to tax rate
+			decimal taxes = subtotal * TAX_RATE;
+
+			// Calculate applicable offers and sum up their discount amounts
+			IDictionary<string, decimal> offers = GetOffers(Products);
+			decimal discounts = 0.0M;
+
+			discounts = offers.Values.Sum();
+
+			// Get total
+			decimal total = subtotal - taxes - discounts;
+
+			// Provide bill
+			return new Bill("$", subtotal, taxes, offers, total);
+        }
+
+		private IDictionary<string, decimal> GetOffers(IEnumerable<Product> products)
+        {
+			Dictionary<string, decimal> offers = new();
+			/* Apply Offer 1: Shoes are on 10% off. */
+			decimal applicableShoeDiscounts = 
+				products.Where(p => p.Name == "Shoes")
+				.Select(p => p.Price * 0.1M)
+				.Sum();
+			
+			if (applicableShoeDiscounts > 0.0M)
+            {
+				offers.Add("10% off shoes", applicableShoeDiscounts);
+            }
+
+			/* Offer 2: Buy two t-shirts and get a jacket half its price.*/
+
+			var jackets = products.Where(p => p.Name == "Jacket");
+			if (jackets.Count() > 0)
+            {
+				int numOfTshirtPairs = products.Where(p => p.Name == "T-shirt").Count() / 2;
+				var numOfJackets = products.Where(p => p.Name == "Jacket").Count();
+				decimal priceOfJacket = jackets.First().Price;
+				
+				// Calculate total discount amount
+				// Ensures number of discounted jackets does not exceed the number of t-shirt pairs bought by the customer.
+				decimal totalJacketDiscountAmount = Math.Min(numOfTshirtPairs, numOfJackets) * priceOfJacket * 0.5M;
+
+				offers.Add("50% off jackets", totalJacketDiscountAmount);
+			}
+
+			return offers;
+		}
 	}
 }
