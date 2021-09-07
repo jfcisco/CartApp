891781753fa854427b64eaabf9f7eb8b0bb77137
Jasper Francisco <jasper.francisco@gmail.com>
2021-09-07 21:03:47 +0800

    Implement a basic product service for CartController
    The values are still hardcoded and will greatly benefit from a persistence layer

---
+++ src/CartApi/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using CartApi.Data;
+using System;
+using System.Linq;
+
+namespace CartApi.Services
+{
+    public class ProductService
+    {
+        private readonly Product[] Products =
+        {
+            new Product { Name = "T-shirt", Price = 10.99M },
+            new Product { Name = "Pants", Price = 14.99M },
+            new Product { Name = "Jacket", Price = 19.99M },
+            new Product { Name = "Shoes", Price = 24.99M }
+        };
+
+        public Product SearchProductByName(string name)
+        {
+            return Products.Where(p => p.Name == name).FirstOrDefault() ?? throw new ProductNotFoundException();
+        }
+    }
+
+    public class ProductNotFoundException : Exception {}
+}
